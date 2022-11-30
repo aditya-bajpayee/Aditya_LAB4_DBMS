1649c3f683f50fa8bfbb55c5dacc47cfbb4e290b
Aditya Bajpayee <110543768+git-catc@users.noreply.github.com>
2022-11-30 19:17:51 +0530

    Update ADITYA_DBMS_LAB.sql

--- ADITYA_DBMS_LAB.sql
+++ ADITYA_DBMS_LAB.sql
@@ -215,7 +215,7 @@ FROM orders
  
    SELECT customer.CUS_NAME,customer.CUS_GENDER
   FROM customer 
-  WHERE customer.CUS_NAME LIKE 'A%' OR customer.CUS_NAME LIKE '%A';
+  WHERE customer.CUS_NAME LIKE 'A%' OR customer.CUS_NAME LIKE 'A%';
   
   
   
